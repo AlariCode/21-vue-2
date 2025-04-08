579bfa084d21c3e738405831b29dc3dff664ea6e
Alaricode <antonlarichev@gmail.com>
2025-04-08 12:40:25 +0300

    children

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TestComp from './components/TestComp.vue';
 import TestComp2 from './components/TestComp2.vue';
+import AuthView from './views/AuthView.vue';
+import MainView from './views/MainView.vue';
 
 export const router = createRouter({
 	routes: [
 		{
-			path: '/', component: TestComp,
+			path: '/', component: AuthView,
 		}, {
-			path: '/new', component: TestComp2,
+			path: '/main', component: MainView,
+			children: [
+				{ path: '', component: TestComp },
+				{ path: 'new', component: TestComp2 },
+			]
 		}
 	],
 	history: createWebHistory()
