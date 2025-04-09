7a97780fe0660dd1e42811ff8664d5cb6613d441
Alaricode <antonlarichev@gmail.com>
2025-04-09 12:55:38 +0300

    Active links

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -15,7 +15,9 @@ onMounted(() => {
 <template>
   <ul class="category-list">
     <li class="list-item" v-for="item in store.categories" :key="item.id">
-      <RouterLink :to="`/main/${item.alias}`">{{ item.name }}</RouterLink>
+      <RouterLink active-class="active-link" :to="`/main/${item.alias}`">{{
+        item.name
+      }}</RouterLink>
     </li>
     <li>
       <ButtonIcon @click="store.createCategory">
@@ -46,7 +48,8 @@ onMounted(() => {
   color: var(--color-fg);
 }
 
-.list-item a:hover {
+.list-item a:hover,
+.list-item a.active-link {
   font-size: 24px;
   font-weight: 700;
 }
